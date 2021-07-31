294e900d1306830feedbb9a43eb4554a1435c481
Rafi Ariel <65024803+RafiAriel@users.noreply.github.com>
2021-07-31 23:02:03 +0300

    Add files via upload

---
+++ weatherWeb/script.js
@@ -0,0 +1,48 @@
+
+
+// need div with id = output 
+function display(element, data, id) {
+    let someEl = document.createElement(element);
+    let text = document.createTextNode(data);
+    someEl.appendChild(text);
+    document.getElementById(id).appendChild(someEl);
+}
+
+
+function get_data(city) {
+    document.getElementById("icon").setAttribute("width", 60);
+    document.getElementById("icon").setAttribute("height", 60);
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', "https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&units=metric&appid=ce3f51ed33b5e660ec79136c46f5dcdb", true);
+    xhr.send();
+    xhr.onload = function () {
+        let Text_data = xhr.responseText;
+        let data = JSON.parse(Text_data);
+        if (xhr.status == 200) {
+            let icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+            let temp = data.main.temp;
+            let weather = data.weather[0].main;
+            document.getElementById("temp").innerHTML = temp + "°C";
+            if(weather === "Clear")
+            {
+                weather = "נעים וחמים";
+                document.getElementById("icon").setAttribute("src", "https://www.pinclipart.com/picdir/middle/115-1155947_sun-logo-clip-art-at-clker-transparent-background.png");
+            }
+            if(weather === "Clouds")
+            {
+                weather = "מעונן";
+                document.getElementById("icon").setAttribute("src", icon);
+            }
+            if(weather === "Rain")
+            {
+                weather = "גשום";
+                document.getElementById("icon").setAttribute("src", icon);
+            }
+
+            document.getElementById("weather").innerHTML = weather;
+
+        }
+
+
+    }
+}
